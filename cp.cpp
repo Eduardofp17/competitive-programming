6aee40ac54865ae6ec9a43f9b48738f8af5086c8
Eduardo Pinheiro <90978242+Eduardofp17@users.noreply.github.com>
2024-07-31 21:36:57 -0300

    Adding basic cpp file

---
+++ cp.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<cstring>
+#include<iomanip>
+#include<utility>
+#include<unordered_set>
+#include<unordered_map>
+#include <queue>
+
+typedef long long ll;
+
+#define _ ios_base::sync_with_stdio(false);cin.tie(0);
+#define endl "\n"
+#define f first
+#define s second
+#define mp make_pair
+#define mt make_tuple
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define pii pair<int, int>
+#define pll pair<ll,ll>
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const double EPS = 1e-7;
+const int MOD = 1e9 + 7;
+
+using namespace std;
+
+
+int main(){_
+
+}
